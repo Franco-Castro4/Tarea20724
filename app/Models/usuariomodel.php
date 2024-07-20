e35c4b70e4c5ef69ec7ecf7a2bb532e2734418d2
Franco Castro <fcastro@estudiante.ceibal.edu.uy>
2024-07-20 14:18:12 -0300

    Se crearon los modelos y controladores de usuarios, se enlazo la base de datos

---
+++ app/Models/usuariomodel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class usuariomodel extends Model
+{
+    use HasFactory;
+}
